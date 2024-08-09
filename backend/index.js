8ab371945d9910aa7ecc0faa95c302c86912fa3a
Aayush Rana <88928434+AaYuShRana14@users.noreply.github.com>
2024-08-09 21:43:57 +0530

    Update index.js
    url fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ async function main() {
 };
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://gofoodd.netlify.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
